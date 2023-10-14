a26eeccc351b198479cf6c89ced2e0e91f02cc5c
ddr2810 <ddr11aqq@outlook.com>
2023-10-15 00:08:07 +0800

    1

--- T5.C
+++ T5.C
@@ -5,7 +5,7 @@ int main()
     int l, m;
     scanf("%d %d", &l, &m);
 
-    int n[l + 1]{0};
+    int n[l + 1]{0}; // 每一项有树时值为0，无树时值为1
 
     for (; m > 0; m--)
     {
@@ -20,7 +20,7 @@ int main()
 
     int sum = 0;
 
-    for (int i = 0; i < l + 1; i++)
+    for (int i = 0; i < l + 1; i++) // 统计0的数量，即为树的数量
     {
         if (n[i] < 1)
             sum++;
